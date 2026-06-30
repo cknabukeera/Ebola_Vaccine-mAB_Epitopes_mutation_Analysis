dee8ee963e98369c570d222d3300834f5841eaec
Kevin Cissy Nabukeera <cknabukeera@kla-ac-hpc-02.ace.ac.ug>
2026-06-30 10:03:44 +0300

    Add ddG scripts for all antibody subsets and GP1+GP2

--- slurm_ddg_calc_GP_REGN3479.sh
+++ slurm_ddg_calc_GP_REGN3479.sh
@@ -15,13 +15,13 @@ export MKL_NUM_THREADS=1
 
 mkdir -p GP_REGN3479
 cd GP_REGN3479 || exit 1
-PDB="../7TN9_GP_REGN3479.pdb"
+PDB="/etc/ace-data/home/cknabukeera/pyrosetta/7TN9/7TN9_GP_REGN3479_with_GP2.pdb"
 # Run mutations on the GP (chain T) in the GP_REGN3479 complex
 # New mutations from REGN3470 (assuming on chain T)
-python3 ../ddg_calcV3.py -p "$PDB" -r 504 -c T -m T --relax_rounds 5 -o result_I504T_subset.csv
-python3 ../ddg_calcV3.py -p "$PDB" -r 505 -c T -m L --relax_rounds 5 -o result_V505L_subset.csv
-python3 ../ddg_calcV3.py -p "$PDB" -r 507 -c T -m S --relax_rounds 5 -o result_N507S_subset.csv
-python3 ../ddg_calcV3.py -p "$PDB" -r 507 -c T -m R --relax_rounds 5 -o result_N507R_subset.csv
+python3 ../ddg_calcV3.py -p "$PDB" -r 504 -c V -m T --relax_rounds 5 -o result_I504T_subset.csv
+python3 ../ddg_calcV3.py -p "$PDB" -r 505 -c V -m L --relax_rounds 5 -o result_V505L_subset.csv
+python3 ../ddg_calcV3.py -p "$PDB" -r 507 -c V -m S --relax_rounds 5 -o result_N507S_subset.csv
+python3 ../ddg_calcV3.py -p "$PDB" -r 507 -c V -m R --relax_rounds 5 -o result_N507R_subset.csv
 
 
 # Combine subset results
